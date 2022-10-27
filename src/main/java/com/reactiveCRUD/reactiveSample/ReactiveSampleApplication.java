b89f1260be412e052df643f838be64e35d63cb3f
Prakash <jpdmprksh@gmail.com>
2022-10-27 20:53:38 +0530

    M::Initial commit

---
+++ src/main/java/com/reactiveCRUD/reactiveSample/ReactiveSampleApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveCRUD.reactiveSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactiveCRUD/reactiveSample/ReactiveSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveCRUD.reactiveSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
